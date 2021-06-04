8536f606700768efc2e59293422829a885d3e175
John Adewumi <johnadewumi63@gmail.com>
2021-06-04 14:46:49 -0700

    new

--- main.tf
+++ main.tf
@@ -253,6 +253,32 @@ data "oci_core_vnic_attachments" "dataSWebserver1_VNIC1_attach"{
   instance_id = oci_core_instance.dataSWebserver1.id
 }
 
+resource "oci_core_instance" "dataSWebserver2" {
+  availability_domain = var.ADs
+  compartment_id = var.compartment
+  display_name = "dataSWebServer2"
+  shape = var.Shape
+  source_details {
+     source_type = "image"
+     source_id   = var.image
+   }
+
+  create_vnic_details {
+     subnet_id = oci_core_subnet.dataSWebSubnet.id
+     assign_public_ip = true
+  }
+
+  metadata = {
+    ssh_authorized_keys = file(var.ssh_authorized_keys)
+  }
+}
+
+data "oci_core_vnic_attachments" "dataSWebserver1_VNIC2_attach"{
+  availability_domain = var.ADs
+  compartment_id = var.compartment
+  instance_id = oci_core_instance.dataSWebserver2.id
+}
+
 variable "region" {
   default = "us-ashburn-1"
 }
